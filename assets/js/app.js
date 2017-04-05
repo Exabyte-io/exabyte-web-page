28319a41caecba1f4c7ad276af08d5d1ced8987f
Timur Bazhirov <timur@exabyte.io>
2017-04-04 22:19:02 -0700

    Adjusted required images for loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,10 +34,11 @@ var loader = (function () {
 
 loader.require([
     'img/airplane-factory.jpg',
-    'img/battery-factory.jpeg',
-    'img/chemical-plant.jpg',
-    'img/semiconductor-fab.jpg',
-    'img/solar-plant.jpg'
+    'img/screencast.gif'
+//    'img/battery-factory.jpeg',
+//    'img/chemical-plant.jpg',
+//    'img/semiconductor-fab.jpg',
+//    'img/solar-plant.jpg'
 ]);
 
 $(document).ready(function () {
