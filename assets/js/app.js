59f58dfaa191ab478b22ca83e8c9c6abbe48b7be
Timur Bazhirov <timur@exabyte.io>
2018-07-23 18:45:10 -0700

    Added notify.js with recent updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,7 @@ var loader = (function () {
                 // generateCompanyList is imported from "./company-list.js"
                 $('#main-content').show().animate({opacity: 1}, 900, generateCompanyList);
                 loadParticleJS();
+                loadnNotifyJs();
             }, 100);
         });
     }
@@ -141,13 +142,13 @@ var changeFunction = function () {
         headTitle;
     // catch links that redirect to
     if (_checkURLlinks($(this),
-            [
-                "platform.exabyte.io",
-                "docs.exabyte.io",
-                "blog.exabyte.io",
-                "exabyte.docsend.com",
-                "angel.co"
-            ])) {
+        [
+            "platform.exabyte.io",
+            "docs.exabyte.io",
+            "blog.exabyte.io",
+            "exabyte.docsend.com",
+            "angel.co"
+        ])) {
         return;
     }
 
@@ -223,7 +224,6 @@ var cssTitleSlider = {
     }
 };
 
-
 // Enable title animation on scrolling
 var onScrollDown = {
     init: function () {
@@ -272,116 +272,154 @@ var formValidation = {
 };
 
 var particleJSConfig = {
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.4,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.2,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.4,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
         "line_linked": {
-          "opacity": 1
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.2,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
         }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
 };
 
 function loadParticleJS() {
     particlesJS(particleJSConfig);
+}
+
+function loadnNotifyJs() {
+
+    //add a new style 'updates'
+    $.notify.addStyle('updates', {
+        html: `
+            <div class='clearfix'>
+                <div class='title exb-color' data-notify-html='title'/>
+                        <button type="button" class="close" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    <div class='buttons'>
+                        <a href="https://arxiv.org/abs/1807.05623" 
+                            target="_blank" class='btn btn-outline no'>Manuscript</a>
+                        <a href="https://platform.exabyte.io/exabyte-io/projects/exabyte-io-2018-bg-study-phase-i" 
+                            target="_blank" class='btn btn-outline no'>Open Data</a>
+                    </div>
+                </div>
+            </div>
+        `
+    });
+
+    //listen for click events from this style
+    $(document).on('click', '.notifyjs-updates-base .close', function () {
+        //programmatically trigger propogating hide event
+        $(this).trigger('notify-hide');
+    });
+
+    $.notify({
+        title: 'Recent updates:',
+    }, {
+        position: "left bottom",
+        style: 'updates',
+        showDuration: 500,
+        autoHide: false,
+        clickToHide: false
+    });
 }
