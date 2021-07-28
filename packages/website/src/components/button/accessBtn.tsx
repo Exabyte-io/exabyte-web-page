e7d2a5e7b9f62e63f832a017a18ed73215952cfe
kulyk <kyluk5@gmail.com>
2021-07-28 15:14:32 +0300

    hero section done without video

--- packages/website/src/components/button/accessBtn.tsx
+++ packages/website/src/components/button/accessBtn.tsx
@@ -1,6 +1,6 @@
 import { CSSProperties, FC } from 'react'
 import { Typography } from 'antd'
-import UnlockSvg from '../../pages/home/images/unlock.svg'
+import UnlockSvg from './unlock.svg'
 import './acessBtn.less'
 
 type AccessBtnProps = {
@@ -9,7 +9,7 @@ type AccessBtnProps = {
 
 const AccessBtn: FC<AccessBtnProps> = ({ style }) => (
   <button style={style} className={'access-btn-wrapper'}>
-    <UnlockSvg style={{ marginRight: '12px' }} />
+    <UnlockSvg style={{ marginRight: 12 }}  className={'access-btn-wrapper-img'}/>
     <Typography.Paragraph className={'access-btn-wrapper-text'}>We support Open Access</Typography.Paragraph>
   </button>
 )
--- packages/website/src/pages/home/sections/Hero.tsx
+++ packages/website/src/pages/home/sections/Hero.tsx
@@ -1,37 +1,34 @@
 import { FC } from 'react'
 import { Layout, Row, Typography } from 'antd'
 import { ActionButton } from '../../../components/button/actionButton'
-import previewImg from '../images/preview-mb.png'
 import HeroImgMb from '../images/home-hero-mb.svg'
 import HeroImgLg from '../images/home-hero-lg.svg'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-// import testVideo from '../../../../public/video/videoplayback.mp4'
+import testVideo from '../../../../public/video/videoplayback.mp4'
 import { useMediaQuery } from 'react-responsive'
 import { AccessBtn } from '../../../components/button/accessBtn'
 import './Hero.less'
 
 const Hero: FC = () => {
   const md = useMediaQuery({ minWidth: '768px' })
-  // const xl = useMediaQuery({ minWidth: '992px' })
+  const xl = useMediaQuery({ minWidth: '992px' })
 
   return (
     <Layout className={'section home-hero'}>
       {!md && <HeroImgMb className={'home-hero-img'} />}
       {md && <HeroImgLg className={'home-hero-img'} />}
-      <Row align={'middle'} justify={'space-between'} className={'container'}>
+      <Row align={'middle'} justify={'space-between'} className={'container section home-hero-absolute'}>
         <Typography.Paragraph className={'home-hero-title'}>Materials Modeling 2.0</Typography.Paragraph>
         <Typography.Paragraph className={'home-hero-description'}>
           Exabyte.io is a cloud-native digital materials R&D platform. Design structures, run simulations, and build
           models online alongside scientists on your team and worldwide.
         </Typography.Paragraph>
-        <ActionButton title={'Get Started Now'} type={'default'} size={'medium'} styles={{ marginBottom: '17px' }} />
-        <AccessBtn style={!md ? { marginBottom: '64px' } : undefined} />
-        {/*hero video */}
-        <video poster={previewImg}>
-          <source/>
-        </video>
-        {/*hero video */}
+        <Row>
+          <ActionButton title={'Get Started Now'} type={'default'} size={'medium'}
+                        styles={xl ? { marginRight: 32 } : { marginBottom: 17 }} />
+          <AccessBtn style={!md ? { marginBottom: 64 } : undefined} />
+        </Row>
       </Row>
     </Layout>
   )
