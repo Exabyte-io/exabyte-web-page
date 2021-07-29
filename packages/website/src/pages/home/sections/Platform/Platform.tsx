17b4b09c9e535605b1f2d47e0d26f15a77ea8317
kyluk <andrii.kyluk@bndigital.co>
2021-07-29 23:51:03 +0300

    created button inside platform section

--- packages/website/src/pages/home/sections/Platform/Platform.tsx
+++ packages/website/src/pages/home/sections/Platform/Platform.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react'
-import { Layout } from 'antd'
+import { Col, Layout, Row, Typography } from 'antd'
 import PlatformBgMb from './images/platform-bg-mb.svg'
 import PlatformBgLg from './images/platform-bg-lg.svg'
 import './Platform.less'
 import { useMediaQuery } from 'react-responsive'
+import { ActionButton } from '../../../../components/button/ActionButton'
 
 const Platform: FC = () => {
   const md = useMediaQuery({ minWidth: 768 })
@@ -12,6 +13,11 @@ const Platform: FC = () => {
   return (
     <Layout className={'section platform-wrapper'}>
       <img src={!md ? PlatformBgMb : PlatformBgLg} alt={'Matera'} className={'home-hero-img'} />
+      <div
+        className={'platform-wrapper-button'}
+      >
+        <ActionButton title={'CREATE A FREE ACCOUNT'} type={'default'} size={'medium'} />
+      </div>
     </Layout>
   )
 }
