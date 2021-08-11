36f45e5290530c84bd9f2008efa4e017afc69543
Igor Goncharenko <ihorhoncharenko8@gmail.com>
2021-08-11 06:16:07 +0300

    Added one section

--- packages/website/src/pages/about/About.tsx
+++ packages/website/src/pages/about/About.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react'
 import { Hero } from './sections/Hero/Hero'
+import { Leadership } from './sections/Leadership/Leadership'
 
 const About: FC = () => {
   return (
     <>
       <Hero />
+      <Leadership />
     </>
   )
 }
