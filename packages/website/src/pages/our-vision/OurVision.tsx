ba5f2942a1519a97964c118956b59d6ee53c018d
vlepekha <vlepekha@student.ucode.world>
2021-08-27 18:54:41 +0300

    started working on the third section

--- packages/website/src/pages/our-vision/OurVision.tsx
+++ packages/website/src/pages/our-vision/OurVision.tsx
@@ -2,14 +2,14 @@ import { FC } from 'react';
 import { Typography } from 'antd';
 import { Title } from './sections/title/Title'
 import { Beliefs } from './sections/beliefs/Beliefs'
+import { Vision } from './sections/vision/Vision'
 import './OurVision.less';
 export const OurVision: FC = () => {
   return (
     <>
       <Title />
       <Beliefs />
+      <Vision />
     </>
-
-
   )
 }
