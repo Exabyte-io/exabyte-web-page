9469aba49098f8cca02df38c95ab1cefb1604398
Igor Goncharenko <ihorhoncharenko8@gmail.com>
2021-08-30 18:36:36 +0300

    latest

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react'
 import { Main } from './sections/Main/Main'
+import { Layout } from 'antd'
 
 const News: FC = () => {
   return (
-    <>
+    <Layout.Content>
       <Main />
-    </>
+    </Layout.Content>
   )
 }
 
--- src/pages/news/sections/Main/Main.tsx
+++ src/pages/news/sections/Main/Main.tsx
@@ -1,27 +1,66 @@
 import { FC } from 'react'
-import { Layout } from 'antd'
-// import { NewsCard } from '../../../../types'
+import { NewsCardType } from '../../../../types'
+import { Input, Row, Typography } from 'antd'
+import IconSearch from './images/icon-search.svg'
+import BackgroundLg from './images/background-lg.svg'
+import NewsCard from './NewsCard/NewsCard'
+import './Main.less'
 
-{
-  /*const newsCards: NewsCard[] = [*/
-}
-//   {
-//     image: '',
-//     title:
-//       'Exabyte.io Tapped by USAF to Give Scientists Real-World Version of Tony Stark’s Futuristic Ironman Simulation Software',
-//     released: 'Released 2020-09-30 via WebWire.',
-//     tags: ['2020-09-30', 'AI', 'ML', 'AFWERX', 'AFRL'],
-//     paragraphs: [
-//       'Exabyte.io, the company unlocking the rapid development of new materials for Global 2000 companies, announces its selection as a $1M Small Business Innovation Research (SBIR) Phase II award recipient from the United States Air Force’s technology incubator, AFWERX. The technology at the center of the award is Exabyte.io’s Elemental Modeling and Manufacturing Architecture (EMMA), a real-world version of the futuristic software that Tony Stark used to create new materials for his “Ironman” suit in the Marvel movie. Exabyte is also actively hiring to grow its team to support the award and other areas of strong growth.',
-//       'The AFWERX is a highly competitive program that encourages domestic small businesses to engage in federal research and research and development with the potential for commercialization. Exabyte.io received the Phase II award to conduct research and development and adapt its software to address the application areas and materials of interest to the Air Force. The Phase II award was granted after successfully completing a Phase I evaluation.',
-//       '“Material requirements for today’s cutting-edge manufacturing are immense and ever-increasing,” explains Timur Bazhirov, Ph.D., CEO and founder of Exabyte.io. “This Phase II award underscores the importance of understanding and exploiting the latest digital R&D methods to keep pace with accelerating materials and manufacturing lifecycles.”',
-//       'The Air Force Research Laboratory and AFWERX work together to streamline the SBIR process and accelerate the development of new technologies, broaden the pool of potential applicants and decrease bureaucratic overhead. Beginning in 2018, the Air Force started offering ’Special’ SBIR topics to drive a broader range of innovations that are faster and leaner.',
-//     ],
-//   },
-// ]
+const newsCards: NewsCardType[] = [
+  {
+    image: '',
+    title:
+      'Exabyte.io Tapped by USAF to Give Scientists Real-World Version of Tony Stark’s Futuristic Ironman Simulation Software',
+    released: 'Released 2020-09-30 via WebWire.',
+    tags: ['2020-09-30', 'AI', 'ML', 'AFWERX', 'AFRL'],
+    paragraphs: [
+      'Exabyte.io, the company unlocking the rapid development of new materials for Global 2000 companies, announces its selection as a $1M Small Business Innovation Research (SBIR) Phase II award recipient from the United States Air Force’s technology incubator, AFWERX. The technology at the center of the award is Exabyte.io’s Elemental Modeling and Manufacturing Architecture (EMMA), a real-world version of the futuristic software that Tony Stark used to create new materials for his “Ironman” suit in the Marvel movie. Exabyte is also actively hiring to grow its team to support the award and other areas of strong growth.',
+      'The AFWERX is a highly competitive program that encourages domestic small businesses to engage in federal research and research and development with the potential for commercialization. Exabyte.io received the Phase II award to conduct research and development and adapt its software to address the application areas and materials of interest to the Air Force. The Phase II award was granted after successfully completing a Phase I evaluation.',
+      '“Material requirements for today’s cutting-edge manufacturing are immense and ever-increasing,” explains Timur Bazhirov, Ph.D., CEO and founder of Exabyte.io. “This Phase II award underscores the importance of understanding and exploiting the latest digital R&D methods to keep pace with accelerating materials and manufacturing lifecycles.”',
+      'The Air Force Research Laboratory and AFWERX work together to streamline the SBIR process and accelerate the development of new technologies, broaden the pool of potential applicants and decrease bureaucratic overhead. Beginning in 2018, the Air Force started offering ’Special’ SBIR topics to drive a broader range of innovations that are faster and leaner.',
+    ],
+  },
+  {
+    image: '',
+    title:
+      'Exabyte.io Tapped by USAF to Give Scientists Real-World Version of Tony Stark’s Futuristic Ironman Simulation Software',
+    released: 'Released 2020-09-30 via WebWire.',
+    tags: ['2020-09-30', 'AI', 'ML', 'AFWERX', 'AFRL'],
+    paragraphs: [
+      'Exabyte.io, the company unlocking the rapid development of new materials for Global 2000 companies, announces its selection as a $1M Small Business Innovation Research (SBIR) Phase II award recipient from the United States Air Force’s technology incubator, AFWERX. The technology at the center of the award is Exabyte.io’s Elemental Modeling and Manufacturing Architecture (EMMA), a real-world version of the futuristic software that Tony Stark used to create new materials for his “Ironman” suit in the Marvel movie. Exabyte is also actively hiring to grow its team to support the award and other areas of strong growth.',
+      'The AFWERX is a highly competitive program that encourages domestic small businesses to engage in federal research and research and development with the potential for commercialization. Exabyte.io received the Phase II award to conduct research and development and adapt its software to address the application areas and materials of interest to the Air Force. The Phase II award was granted after successfully completing a Phase I evaluation.',
+      '“Material requirements for today’s cutting-edge manufacturing are immense and ever-increasing,” explains Timur Bazhirov, Ph.D., CEO and founder of Exabyte.io. “This Phase II award underscores the importance of understanding and exploiting the latest digital R&D methods to keep pace with accelerating materials and manufacturing lifecycles.”',
+      'The Air Force Research Laboratory and AFWERX work together to streamline the SBIR process and accelerate the development of new technologies, broaden the pool of potential applicants and decrease bureaucratic overhead. Beginning in 2018, the Air Force started offering ’Special’ SBIR topics to drive a broader range of innovations that are faster and leaner.',
+    ],
+  },
+]
 
 const Main: FC = () => {
-  return <Layout className='news-main'>News</Layout>
+  return (
+    <div className='news'>
+      <div className='content'>
+        <Row>
+          <Typography.Title>News</Typography.Title>
+          <div className='input-wrapper'>
+            <Input placeholder='Search' />
+            <img src={IconSearch} alt='' />
+          </div>
+        </Row>
+        <Row>
+          {newsCards.map((card, index) => (
+            <NewsCard
+              key={index}
+              image={card.image}
+              title={card.title}
+              paragraphs={card.paragraphs}
+              tags={card.tags}
+              released={card.released}
+            />
+          ))}
+        </Row>
+      </div>
+    </div>
+  )
 }
 
 export { Main }
