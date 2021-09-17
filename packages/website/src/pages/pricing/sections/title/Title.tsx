b98cbefb73fe8b9a0f40bb78e3ae04d6ac4a3e5d
bn-enginseer <dev@bndigital.co>
2021-09-17 23:10:30 +0300

    created new hero section inside pricing page & yarn fix

--- packages/website/src/pages/pricing/sections/title/Title.tsx
+++ packages/website/src/pages/pricing/sections/title/Title.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
+import bgLg from './title-bg-lg.svg'
+import bgMb from './title-bg-mb.svg'
+import './Title.less'
 import { Typography } from 'antd'
-import '../../../our-vision/sections/title/Title.less'
 import { useContentQuery } from '../../../../graphql'
 
 export const Title: FC = () => {
@@ -12,14 +14,22 @@ export const Title: FC = () => {
   const content = data?.content
 
   return (
-    <section className={'title-section'}>
-      <Typography.Title className={'page-title'}>{content?.title}</Typography.Title>
-      <p className={'subtitle'} style={{ maxWidth: 880 }}>
-        {content?.description}
-      </p>
-      <p className={'content'} style={{ paddingRight: 170 }}>
-        {content?.subtitle}
-      </p>
+    // <section className={'title-section'}>
+    //   <Typography.Title className={'page-title'}>{content?.title}</Typography.Title>
+    //   <p className={'subtitle'} style={{ maxWidth: 880 }}>
+    //     {content?.description}
+    //   </p>
+    //   <p className={'content'} style={{ paddingRight: 170 }}>
+    //     {content?.subtitle}
+    //   </p>
+    // </section>
+    <section className={'title'}>
+      <div className={'container title-content'}>
+        <Typography.Paragraph className={'title-content-title'}>{content?.title}</Typography.Paragraph>
+        <Typography.Paragraph className={'title-content-description'}>{content?.description}</Typography.Paragraph>
+      </div>
+      <img className={'title-bg-lg'} src={bgLg} alt={'image'} />
+      <img className={'title-bg-mb'} src={bgMb} alt={'image'} />
     </section>
   )
 }
