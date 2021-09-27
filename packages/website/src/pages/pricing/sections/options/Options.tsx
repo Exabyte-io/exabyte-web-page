52b3360f5ee923e4f563b5359fbdc18deeba6f0c
kyluk5 <kyluk5@gmail.com>
2021-09-27 15:05:01 +0300

    after style change inside options section

--- packages/website/src/pages/pricing/sections/options/Options.tsx
+++ packages/website/src/pages/pricing/sections/options/Options.tsx
@@ -1,12 +1,8 @@
 import { FC } from 'react'
-import Image from './image/image.svg'
 import './Options.less'
 import { useContentQuery } from '../../../../graphql'
-import { useMediaQuery } from 'react-responsive'
 
 export const Options: FC = () => {
-  const xl = useMediaQuery({ minWidth: '992px' })
-
   const { data } = useContentQuery({
     variables: {
       slug: 'pricing-content',
@@ -16,10 +12,7 @@ export const Options: FC = () => {
 
   return (
     <section className={'option-section'}>
-      <p className={'option-title'}>
-        {xl && <img src={Image} alt={'image'} />}
-        {content?.title}
-      </p>
+      <p className={'option-title'}>{content?.title}</p>
       <p className={'option-text'}>{content?.subTitle}</p>
     </section>
   )
