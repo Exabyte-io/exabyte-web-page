3d8c0c94b0aad3a6a51d91de5c4fca3e40f80431
kyluk5 <kyluk5@gmail.com>
2021-09-27 14:48:23 +0300

    after new style changes cost structure section according to new layout

--- packages/website/src/pages/pricing/sections/cost-structure/CostStructure.tsx
+++ packages/website/src/pages/pricing/sections/cost-structure/CostStructure.tsx
@@ -3,6 +3,9 @@ import { Col, Row } from 'antd'
 import '../../../our-vision/sections/beliefs/Beliefs.less'
 import './CostStructure.less'
 import { useContentQuery } from '../../../../graphql'
+import One from './images/1.svg'
+import Two from './images/2.svg'
+import Three from './images/3.svg'
 
 export const CostStructure: FC = () => {
   const { data } = useContentQuery({
@@ -20,13 +23,12 @@ export const CostStructure: FC = () => {
       </div>
       <div className={'container'}>
         <Row gutter={{ sm: 0, md: 151 }}>
-          {content?.sections?.map(it => (
+          {content?.sections?.map((it, index) => (
             <Col sm={24} md={8}>
               <article className={'item'}>
-                <div className={'number'}>
-                  <span>{it?.title} </span>
-                  {/*<span className={'cost-title'}>{it?.subTitle}</span>*/}
-                </div>
+                {index === 0 && <img src={One} alt={'image'} />}
+                {index === 1 && <img src={Two} alt={'image'} />}
+                {index === 2 && <img src={Three} alt={'image'} />}
                 <div className={'belief'}>{it?.description}</div>
               </article>
             </Col>
